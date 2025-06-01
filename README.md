125580cf9fa19c39b9647bc18216ac01cc1fc1af
Duncan Davidson <duncan@Blackbird.local>
2025-06-01 07:17:21 +0200

    Add comprehensive setup script and detailed README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# dotfiles
+# duncan does dotfiles
+
+Dotfiles are how you personalize a system. These are mine. They’ve changed a lot
+over the years. They’ll probably keep changing. That's the way of it all, innit?
+
+One of the biggest changes recently is the move from Homebrew to
+[Flox](https://flox.dev). This is thanks to my time at Shopify where [Tobi
+Lütke](https://github.com/tobi) and Burke Libbey turned me onto Nix and it’s
+ability to maintain different tooling configs on a per-project basis.
+
+# Install
+
+I clone my dotfiles repo into `~/src` because it’s under source code management
+like everything else. Others clone their dotfiles into `~/.dotfiles`. Whatever
+floats your boat.
+
+The “safe” way to do this is:
+
+```
+mkdir -p ~/src
+git clone https://github.com/duncan/dotfiles ~/src/dotfiles
+cd ~/src/dotfiles
+./install.sh
+```
+
+Or, you can go full out and do that thing that we really don’t suggest folks do
+and pipe the setup script right off of GitHub and into your shell:
+
+```
+curl https://raw.githubusercontent.com/duncan/dotfiles/main/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,7 +1,49 @@
 #/bin/bash
 
-cd `dirname $0`
+# BOOTSTRAP
 
-# LINK DOTFILES
+if [ -d $HOME/src/dotfiles ] ; then
+  echo "✅ Dotfiles repo accounted for!"
+else
+  echo "Starting from bare metal, I see. Daring! Let’s go!"
+  mkdir -p $HOME/src
+  git clone https://github.com/dotfiles
+  if [ -d $HOME/src/dotfiles ] ; then
+    echo "✅ Dotfiles repo cloned!"
+  else
+    echo "⛔️ Dotfiles repo wasn’t cloned successfully!"
+    exit -1
+  fi
+  # Let's execute and exit the status
+fi
+
+cd $HOME/src/dotfiles
+
+# FLOX
+#
+# Since we use Flox (a Nix wrapper)
+
+if [ ! -f /usr/local/bin/flox ] ; then
+  curl -L https://flox.dev/downloads/osx/flox.aarch64-darwin.pkg | sudo installer -pkg /dev/stdin -target /
+  if  [ -f /usr/local/bin/flox ] ; then
+    echo "✅ Flox installed successfully!"
+  else
+    echo "⛔️ Flox didn’t install successfully!"
+    exit -1
+  fi
+else
+  echo "✅ Flox is already installed!"
+fi
+
+# DOTFILES
+#
+# Ok, this is the pretty boring part. Just link things up and we’re done.
+
 ln -nfs `realpath editorconfig` $HOME/.editorconfig
 ln -nfs `realpath zshrc` $HOME/.zshrc
+mkdir -p $HOME/.config/zed
+ln -nfs `realpath zed.settings.json` $HOME/.config/zed/settings.json
+
+# SAY GOODBYE
+
+echo "🤖 You’re set up, buckaroo. Let’s go and make something awesome!"
